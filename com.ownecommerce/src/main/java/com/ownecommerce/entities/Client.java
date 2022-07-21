207be9a67f1f0eb1370bafe00db2fc8d679d2c23
wallyson ruan <wallysonruan@gmail.com>
2022-07-21 11:13:58 -0300

    Created the Client resource and repository

--- com.ownecommerce/src/main/java/com/ownecommerce/entities/Client.java
+++ com.ownecommerce/src/main/java/com/ownecommerce/entities/Client.java
@@ -1,5 +1,65 @@
 package com.ownecommerce.entities;
 
-public class Client {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	
+	public Client(Long id, String name, String cpf) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", cpf=" + cpf + "]";
+	}
 }
